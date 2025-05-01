b4c5d819b876759405fc2150a7ac832e7c1f6172
Tymasc95 <tyler.mascitelli@gmail.com>
2025-04-30 23:16:19 -0400

    Edited script to write and load tasks dependent on choice

--- osrs_decider.py
+++ osrs_decider.py
@@ -6,7 +6,8 @@ import sys
 raids = ["ToB", "ToA", "CoX"]
 bosses = ["Duke", "Zulrah", "Bandos", "Vorkath", "Artio", "Muspah", "Mole", "Vardorvis", "Huey", "Titans"]
 choices = ["Skilling", "Boss", "Raid", "Slayer", "GET THAT WARHAMMER", "Extra bloodshard?", "Corrupted Gauntlet"]
-penalties = ["Tell Joe he's handsome"]
+
+file_name = "osrs_decider_log.txt"
 
 # User input to kick off function
 user_input = input("What's the matter stupid, don't know what to do?: ")
@@ -21,7 +22,7 @@ print("What would you like to do?:")
 time.sleep(1)
 print("!new : to roll a new task")
 time.sleep(0.5)
-print("!current : to view current task")
+print("!task : to view current task")
 time.sleep(1)
 decision_input = input("Choice: ")
 
@@ -47,111 +48,104 @@ if decision_input.lower() == "!new":
 
     # creates the illusion of loading if decision_input == "!new"
     loading_flair("Choosing new task....")
-
     print()
-
+    print()
     time.sleep(1)
 
     # main function to determine choice
     def osrs_pick():
         result = random.choice(choices)
         match result:
-            # If the function rolls Boss, added loading flair for effect
+            # If the function rolls Boss
             case "Boss":
                 chosen_boss = random.choice(bosses)
-                print("Bosses rolled:", end=" ")
-                loading_flair("Picking Boss....")
-                print()
-                time.sleep(1.2)
-                print(f"Boss to kill is: {chosen_boss}")
-                time.sleep(0.8)
-                print(f"You must kill {random.randrange(10,50,1)} of {chosen_boss}")
-            # If the function rolls Raid, added loading flair for effect  
+                # print("Bosses rolled:", end=" ")
+                # loading_flair("Picking Boss....")
+                # print()
+                # time.sleep(1.2)
+                # print(f"Boss to kill is: {chosen_boss}")
+                # time.sleep(0.8)
+                # print(f"You must kill {random.randrange(10,50,1)} of {chosen_boss}")
+                return f"""Bosses rolled:
+Boss to kill is: {chosen_boss}
+You must kill {random.randrange(10,50,1)} of {chosen_boss}"""
+            # If the function rolls Raid  
             case "Raid":
                 random_raid = random.choice(raids)
-                print("Raids rolled:", end=" ")
-                loading_flair("Picking Raid....")
-                print()
-                time.sleep(1.2)
-                print(f"Raid to run is: {random_raid}")
+                # print("Raids rolled:", end=" ")
+                # loading_flair("Picking Raid....")
+                # print()
+                # time.sleep(1.2)
+                # print(f"Raid to run is: {random_raid}")
+                return f"""Raids rolled:
+Raid to run is: {random_raid}"""
             # Any other choice besides Raid and Boss
             case _:
-                    print(f"Your OSRS task is: {result}")
-                    time.sleep(0.8)
-                    
-    osrs_pick()
-    time.sleep(0.5)
+                # print(f"Your OSRS task is: {result}")
+                return f"""Your OSRS task is: {result}"""
+                time.sleep(0.8)
+    task = osrs_pick()
+    
+    def task_saver(input, filename):
+        with open(filename, 'w') as f:
+            f.write(str(input))
+    task_saver(task, file_name)
+    print("Here is your new task!:")
+    time.sleep(1)
+    print(task)
+    
+elif decision_input.lower() == "!task":
     print()
+    time.sleep(1)
+    def task_loader(filename):
+        with open(filename, 'r') as f:
+            read = f.read()
+        return read
+    current_task = task_loader(file_name)
+    print("This is your current task:")
+    time.sleep(1)
+    print(current_task)
+    # time.sleep(4)
+    # print()
 
-    # Function to roll a new task if user chooses to re-roll
-    def penalty_picker():
-        p_result = random.choice(penalties)
-        print("You have the option to re-roll this choice for a penalty")
-        time.sleep(1)
-        new_input = input("Would you like to re-roll your choice?(Y/N) ")
-        # If user chooses Y it will loop penalties and tasks until satisfied
-        while new_input.lower() == "y":
-            print()
-            print("Penalty accepted:", end=" ")
-            loading_flair("Rolling penalty....")
-            print()
-            time.sleep(1)
-            print(f"Your penalty is {p_result}")
-            time.sleep(1)
-            print("Rolling new task", end="")
-            loading_flair("....")
-            print()
-            time.sleep(1)
-            osrs_pick()
-            new_input = input("Would you like to re-roll your choice?(Y/N) ")
-            time.sleep(0.5)
-        else:
-            print("Good luck, Have fun")
-            time.sleep(3)
-
-    penalty_picker()
-
-    # Function to generate a new task and count completed tasks
-    def new_task():
-        completed_counter = 0
+    # # Function to generate a new task and count completed tasks
+    # def new_task():
+    #     completed_counter = 0
         
-        # While any if statements return true, loops creating new tasks until user chooses not to
-        while True:
-            completed_input = input("Have you completed your task?(Y/N): ")
-            time.sleep(0.5)
-            # If yes adds to counter for completed tasks and asks to roll a new task
-            if completed_input.lower() == "y":
-                completed_counter += 1
-                time.sleep(0.3)
-                print(f"Number of tasks completed: {completed_counter}")
-                time.sleep(1)
+    #     # While any if statements return true, loops creating new tasks until user chooses not to
+    #     while True:
+    #         completed_input = input("Have you completed your task?(Y/N): ")
+    #         time.sleep(0.5)
+    #         # If yes adds to counter for completed tasks and asks to roll a new task
+    #         if completed_input.lower() == "y":
+    #             completed_counter += 1
+    #             time.sleep(0.3)
+    #             print(f"Number of tasks completed: {completed_counter}")
+    #             time.sleep(1)
 
-                new_task_input = input("Would you like a new task?(Y/N): ")
-                print()
-                # If yes to a new task generates a new task and asks for penalty re-roll
-                if new_task_input.lower() == "y":
-                    print("Generating new task:", end="")
-                    loading_flair("....")
-                    print()
-                    osrs_pick()
-                    time.sleep(0.8)
-                    print()
-                    penalty_picker()
-                    print()
-                    time.sleep(3)
-                # If no for generating a new task script ends
-                else:
-                    print("Ending script")
-                    sys.exit()
-            # If has not been completed yet loops back to ask again until task is completed
-            elif completed_input.lower() == "n":
-                print("No problem, I will ask again later. Get to it.")
-                time.sleep(3)
-                print()
-            # Safeguard in case any other option besides Y or N is picked
-            else:
-                print("Please enter Y or N.")
-    new_task()
+    #             new_task_input = input("Would you like a new task?(Y/N): ")
+    #             print()
+    #             # If yes to a new task generates a new task and asks for penalty re-roll
+    #             if new_task_input.lower() == "y":
+    #                 print("Generating new task:", end="")
+    #                 loading_flair("....")
+    #                 print()
+    #                 osrs_pick()
+    #                 print()
+    #                 time.sleep(3)
+    #             # If no for generating a new task script ends
+    #             else:
+    #                 print("Ending script")
+    #                 sys.exit()
+    #         # If has not been completed yet loops back to ask again until task is completed
+    #         elif completed_input.lower() == "n":
+    #             print("No problem, I will ask again later. Get to it.")
+    #             time.sleep(3)
+    #             print()
+    #         # Safeguard in case any other option besides Y or N is picked
+    #         else:
+    #             print("Please enter Y or N.")
+    # new_task()
 
 
        
