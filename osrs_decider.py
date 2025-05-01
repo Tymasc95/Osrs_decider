f8c6d476f3a02935d84336966b982877589cad29
Tymasc95 <tyler.mascitelli@gmail.com>
2025-05-01 11:59:00 -0400

    created new counter file to create persistent counter for completed tasks, cleaned up code

--- osrs_decider.py
+++ osrs_decider.py
@@ -8,6 +8,7 @@ bosses = ["Duke", "Zulrah", "Bandos", "Vorkath", "Artio", "Muspah", "Mole", "Var
 choices = ["Skilling", "Boss", "Raid", "Slayer", "GET THAT WARHAMMER", "Extra bloodshard?", "Corrupted Gauntlet"]
 
 file_name = "osrs_decider_log.txt"
+counter_file_name = "osrs_decider_counter.txt"
 
 # function to add a little flair to make it look like it's loading
 def loading_flair(text, delay=0.4):
@@ -25,11 +26,27 @@ def loading_flair(text, delay=0.4):
         sys.stdout.write(char)
         sys.stdout.flush()
         
+def task_saver(input, filename):
+    with open(filename, 'w') as f:
+         f.write(str(input))
+         
+def task_loader(filename):
+    try:
+        with open(filename, 'r') as f:
+            read = f.read()
+        return read
+    except FileNotFoundError:
+        return "No saved task found. Use !new to generate one."
+    
+completed_tasks = int(task_loader(counter_file_name))
+        
 loading_flair("Booting up OSRS Decider....")
 print()
 print()
 time.sleep(1)
 print("Welcome to FishpasteRS's OSRS Task Decider!")
+time.sleep(0.8)
+print(f"Current number of completed tasks: {completed_tasks}")
 print()
 time.sleep(0.8)
 print("What would you like to do?:")
@@ -50,42 +67,66 @@ def osrs_pick():
         # If the function rolls Boss
         case "Boss":
             chosen_boss = random.choice(bosses)
-            # print("Bosses rolled:", end=" ")
-            # loading_flair("Picking Boss....")
-            # print()
-            # time.sleep(1.2)
-            # print(f"Boss to kill is: {chosen_boss}")
-            # time.sleep(0.8)
-            # print(f"You must kill {random.randrange(10,50,1)} of {chosen_boss}")
             return f"""Bosses rolled:
 Boss to kill is: {chosen_boss}
 You must kill {random.randrange(10,50,1)} of {chosen_boss}"""
         # If the function rolls Raid  
         case "Raid":
             random_raid = random.choice(raids)
-            # print("Raids rolled:", end=" ")
-            # loading_flair("Picking Raid....")
-            # print()
-            # time.sleep(1.2)
-            # print(f"Raid to run is: {random_raid}")
             return f"""Raids rolled:
 Raid to run is: {random_raid}"""
         # Any other choice besides Raid and Boss
         case _:
-            # print(f"Your OSRS task is: {result}")
             return f"""Your OSRS task is: {result}"""
         
-def task_saver(input, filename):
-    with open(filename, 'w') as f:
-         f.write(str(input))
-         
-def task_loader(filename):
-    try:
-        with open(filename, 'r') as f:
-            read = f.read()
-        return read
-    except FileNotFoundError:
-        return "No saved task found. Use !new to generate one."
+def roll_new_task():
+    global completed_tasks   
+    # While any if statements return true, loops creating new tasks until user chooses not to
+    while True:
+        completed_input = input("Have you completed your task?(Y/N): ")
+        print()
+        time.sleep(0.5)
+        # If yes adds to counter for completed tasks and asks to roll a new task
+        if completed_input.lower() == "y":
+            completed_tasks += 1
+            time.sleep(0.3)
+            print(f"Number of tasks completed: {completed_tasks}")
+            task_saver(str(completed_tasks), counter_file_name)
+            time.sleep(1)
+            new_task_input = input("Would you like a new task?(Y/N): ")
+            print()
+            # If yes to a new task generates a new task and overwrites old task
+            if new_task_input.lower() == "y":
+                print("Generating new task:", end="")
+                loading_flair("....")
+                print()
+                time.sleep(1)
+                task = osrs_pick()
+                task_saver(task, file_name)
+                print(task)
+                print()
+                time.sleep(3)
+            # If no for generating a new task script ends
+            else:
+                print("Ending script")
+                sys.exit()
+        # If has not been completed yet loops back to ask again until task is completed
+        elif completed_input.lower() == "n":
+            end_input = input("Would you like to close the decider and report completion later?(Y/N): ")
+            if end_input.lower() == "y":
+                print("Okay! Please use !task when loading the program again")
+                time.sleep(0.8)
+                print()
+                print("Ending script")
+                sys.exit()
+            elif end_input.lower() == "n":
+                print("No problem, I will check again in a bit.")   
+                time.sleep(3)
+                print()
+        # Safeguard in case any other option besides Y or N is picked
+        else:
+            print("Please enter Y or N.")
+    
 
 if decision_input.lower() == "!new":
     
@@ -106,45 +147,6 @@ if decision_input.lower() == "!new":
     print()
     time.sleep(4)
     
-    def roll_new_task():
-        completed_counter = 0
-        
-        # While any if statements return true, loops creating new tasks until user chooses not to
-        while True:
-            completed_input = input("Have you completed your task?(Y/N): ")
-            time.sleep(0.5)
-            # If yes adds to counter for completed tasks and asks to roll a new task
-            if completed_input.lower() == "y":
-                completed_counter += 1
-                time.sleep(0.3)
-                print(f"Number of tasks completed: {completed_counter}")
-                time.sleep(1)
-
-                new_task_input = input("Would you like a new task?(Y/N): ")
-                print()
-                # If yes to a new task generates a new task and asks for penalty re-roll
-                if new_task_input.lower() == "y":
-                    print("Generating new task:", end="")
-                    loading_flair("....")
-                    print()
-                    time.sleep(1)
-                    task = osrs_pick()
-                    task_saver(task, file_name)
-                    print(task)
-                    print()
-                    time.sleep(3)
-                # If no for generating a new task script ends
-                else:
-                    print("Ending script")
-                    sys.exit()
-            # If has not been completed yet loops back to ask again until task is completed
-            elif completed_input.lower() == "n":
-                print("No problem, I will ask again later. Get to it.")
-                time.sleep(3)
-                print()
-            # Safeguard in case any other option besides Y or N is picked
-            else:
-                print("Please enter Y or N.")
     roll_new_task()
     
 elif decision_input.lower() == "!task":
@@ -161,45 +163,6 @@ elif decision_input.lower() == "!task":
     time.sleep(4)
     print()
 
-    # Function to generate a new task and count completed tasks
-    def roll_new_task():
-        completed_counter = 0
-        
-        # While any if statements return true, loops creating new tasks until user chooses not to
-        while True:
-            completed_input = input("Have you completed your task?(Y/N): ")
-            time.sleep(0.5)
-            # If yes adds to counter for completed tasks and asks to roll a new task
-            if completed_input.lower() == "y":
-                completed_counter += 1
-                time.sleep(0.3)
-                print(f"Number of tasks completed: {completed_counter}")
-                time.sleep(1)
-                new_task_input = input("Would you like a new task?(Y/N): ")
-                print()
-                # If yes to a new task generates a new task and asks for penalty re-roll
-                if new_task_input.lower() == "y":
-                    print("Generating new task:", end="")
-                    loading_flair("....")
-                    print()
-                    time.sleep(1)
-                    task = osrs_pick()
-                    task_saver(task, file_name)
-                    print(task)
-                    print()
-                    time.sleep(3)
-                # If no for generating a new task script ends
-                else:
-                    print("Ending script")
-                    sys.exit()
-            # If has not been completed yet loops back to ask again until task is completed
-            elif completed_input.lower() == "n":
-                print("No problem, I will ask again later. Get to it.")
-                time.sleep(3)
-                print()
-            # Safeguard in case any other option besides Y or N is picked
-            else:
-                print("Please enter Y or N.")
     roll_new_task()
 
 
